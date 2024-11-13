069de155012f133930a88ca4bbcfc674e3d1f0b3
Felix Scherz <felixwscherz@gmail.com>
2024-11-13 19:06:01 +0100

    feat: somehow this actually works....

--- years/2020/day01/part1.py
+++ years/2020/day01/part1.py
@@ -1,5 +1,11 @@
-from aoc import get_input, submit
-def test_solution():
-    input = get_input(2020, 1)
-    answer = "abc"
+def main():
+    with open("input.txt") as handle:
+        input = handle.read().splitlines()
 
+    for a in input:
+        for b in input:
+            if int(a) + int(b) == 2020:
+                print(int(a)*int(b))
+                return
+
+main()
