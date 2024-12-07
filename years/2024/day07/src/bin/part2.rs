7c4fbc43fa189a6d19b3ccdc9abc2e00c297a5de
Felix Scherz <felixwscherz@gmail.com>
2024-12-07 11:08:37 +0100

    feat: solve 2024/day07 part2

---
+++ years/2024/day07/src/bin/part2.rs
@@ -0,0 +1,81 @@
+use std::{
+    io::{stdin, Read},
+    str::FromStr,
+};
+
+struct Equation {
+    pub goal: u64,
+    parts: Vec<u64>,
+}
+
+#[derive(Debug)]
+struct ParseEquationError;
+
+impl FromStr for Equation {
+    type Err = ParseEquationError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts_s = s.split(":");
+        let goal = parts_s.next().unwrap().parse().unwrap();
+        let mut parts = vec![];
+        for part_s in parts_s.next().unwrap().split_whitespace() {
+            parts.push(part_s.parse().unwrap());
+        }
+
+        Ok(Self { goal, parts })
+    }
+}
+enum Op {
+    ADD,
+    MULTIPLY,
+    CONCAT,
+}
+
+impl Equation {
+    fn valid(&self) -> bool {
+        // can this be done in a reduce like way?
+        // for a given acc and the rest of the vec c
+        fn combine(goal: u64, acc: u64, op: Op, mut operands: Vec<u64>) -> bool {
+            if operands.is_empty() {
+                return acc == goal;
+            }
+
+            let new_acc = match op {
+                Op::ADD => acc + operands.remove(0),
+                Op::MULTIPLY => acc * operands.remove(0),
+                Op::CONCAT => {
+                    let mut acc_s = acc.to_string();
+                    acc_s.push_str(&operands.remove(0).to_string());
+                    acc_s.parse().unwrap()
+                }
+            };
+
+            if new_acc > goal {
+                return false;
+            }
+
+            return combine(goal, new_acc, Op::ADD, operands.clone())
+                || combine(goal, new_acc, Op::MULTIPLY, operands.clone())
+                || combine(goal, new_acc, Op::CONCAT, operands.clone());
+        }
+
+        let mut operands = self.parts.clone();
+        let acc = operands.remove(0);
+        combine(self.goal, acc, Op::ADD, operands.clone())
+            || combine(self.goal, acc, Op::MULTIPLY, operands.clone())
+            || combine(self.goal, acc, Op::CONCAT, operands.clone())
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+
+    let result: u64 = input
+        .lines()
+        .map(|x| x.parse::<Equation>().unwrap())
+        .filter(|x| x.valid())
+        .map(|x| x.goal)
+        .sum();
+    println!("{}", result);
+}
