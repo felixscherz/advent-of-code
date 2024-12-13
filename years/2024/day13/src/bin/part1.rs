6c4e1e77f07b69e1a43002f2541e2fdd02634e88
Felix Scherz <felixwscherz@gmail.com>
2024-12-13 13:13:33 +0100

    fix: bug in checking whether machine can be won or not

--- years/2024/day13/src/bin/part1.rs
+++ years/2024/day13/src/bin/part1.rs
@@ -23,14 +23,16 @@ impl Point {
         Self { x, y }
     }
 
-    fn reachable_from(&self, other: &Point, moves: &(Move, Move)) -> bool {
+    fn reachable_from(&self, other: &Point, moves: (&Move, &Move)) -> bool {
         // calculate the linear combination and check whether components are integers
-        let target = Self::new(other.x - self.x, other.y - self.y);
+        let target = Self::new(self.x - other.x, self.y - other.y);
         let move_a = &moves.0;
         let move_b = &moves.1;
         let b: f32 = (target.y as f32 - target.x as f32 * move_a.y as f32 / move_a.x as f32)
             / (move_b.y as f32 - move_a.y as f32 * move_b.x as f32 / move_a.x as f32);
         let a: f32 = (target.x as f32 - b * move_b.x as f32) / move_a.x as f32;
+        dbg!(a);
+        dbg!(b);
         (a as f32).fract() == 0f32 && (b as f32).fract() == 0f32 && a >= 0f32 && b >= 0f32
     }
 }
@@ -42,14 +44,16 @@ struct Machine {
     prize: Point,
 }
 
+impl Machine {
+    fn winnable(&self) -> bool {
+        let start = Point::new(0, 0);
+        self.prize.reachable_from(&start, (&self.a, &self.b))
+    }
+}
+
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-
-
-
-
-
 
     // algorithm will be to add nodes all around the starting location at every point reachable
     // until that point cloud contains the prize
@@ -64,25 +68,23 @@ mod test {
     fn test_reachable() {
         let start = Point::new(1, 1);
         let target = Point::new(5, 5);
-        let moves = (Move::new(1, 0), Move::new(0, 1));
-        assert!(target.reachable_from(&start, &moves));
+        let moves = (&Move::new(1, 0), &Move::new(0, 1));
+        assert!(target.reachable_from(&start, moves));
     }
 
     #[test]
     fn test_not_reachable_due_to_fraction() {
         let start = Point::new(0, 0);
         let target = Point::new(5, 5);
-        let moves = (Move::new(2, 0), Move::new(0, 2));
-        assert!(!target.reachable_from(&start, &moves));
+        let moves = (&Move::new(2, 0), &Move::new(0, 2));
+        assert!(!target.reachable_from(&start, moves));
     }
 
     #[test]
     fn test_not_reachable_due_to_wrong_direction() {
         let start = Point::new(0, 0);
         let target = Point::new(-4, 4);
-        let moves = (Move::new(2, 0), Move::new(0, 2));
-        assert!(!target.reachable_from(&start, &moves));
+        let moves = (&Move::new(2, 0), &Move::new(0, 2));
+        assert!(!target.reachable_from(&start, moves));
     }
-
 }
-
